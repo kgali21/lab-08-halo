86ddc41714c427f6e088b40b206acb433ed20b0a
Kamran Ali <kgali21@gmail.com>
2019-08-23 15:48:49 -0700

    start of css

--- public/src/components/services/character.js
+++ public/src/components/services/character.js
@@ -10,28 +10,28 @@ const people = [
         name: 'Master Chief',
         number_games: 6,
         player_character: true,
-        games: ['Halo: Combat Evolved', 'Halo 2', 'Halo 3', 'Halo 4', 'Halo 5: Guardians', 'Halo: Reach'],
+        games: 'Halo: Combat Evolved',
         img: '../assets/imgs/master chief.png'
     },
     {
         name: 'Arbiter',
         number_games: 4,
         player_character: true,
-        games: ['Halo 2', 'Halo 3', 'Halo Wars', 'Halo 5: Guardians'],
+        games: 'Halo 2',
         img: '../assets/imgs/arbiter.png'
     },
     {
         name: 'Cortana',
         number_games: 6,
         player_character: false,
-        games: ['Halo: Combat Evolved', 'Halo 2', 'Halo 3', 'Halo 4', 'Halo 5: Gaurdians', 'Halo: Reach'],
+        games: 'Halo: Combat Evolved',
         img: '../assets/imgs/cortana.png'
     },
     {
         name: 'Guilty Spark',
         number_games: 3,
         player_character: false,
-        games: ['Halo: Combat Evolved', 'Halo 2', 'Halo 3'],
+        games: 'Halo: Combat Evolved',
         img: '../assets/imgs/guilty spark.png'
     },    
 ];
