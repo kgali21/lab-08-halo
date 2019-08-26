dc613c3e55f8f1fabdf261f94af6d126fdc944b7
Kamran Ali <kgali21@gmail.com>
2019-08-26 13:45:14 -0700

    figuring out details page

--- public/src/components/characters/CharacterCards.js
+++ public/src/components/characters/CharacterCards.js
@@ -6,18 +6,20 @@ class CharacterCard extends Component {
 
         return /*html*/`
         <li class="character-container">
-        <div class="name-container">
-            <h2 class="name">${character.name}</h2>
-            <p class="number-games">Total Games: ${character.number_games}</p>
-            <p class="playable-character">Playable Character: ${character.player_character}</p>
-        </div>
-        <div class="image-container">
-            <img src="${character.img}" alt="${character.name}">
-        </div>
-        <div class="games-container">
-            <h3>Game Appearances</h3>
-            <p class="game-list">${character.game}</p>
-        </div>
+            <a href="character-details.html?id=${character.id}" class="detail-link">
+                <div class="name-container">
+                    <h2 class="name">${character.name}</h2>
+                        <p class="number-games">Total Games: ${character.number_games}</p>
+                        <p class="playable-character">Playable Character: ${character.player_character}</p>
+                </div>
+                <div class="image-container">
+                    <img src="${character.img}" alt="${character.name}">
+                </div>
+                <div class="games-container">
+                    <h3>Game Appearances</h3>
+                        <p class="game-list">${character.game}</p>
+                </div>
+            </a>
         </li>
         `;
     }
